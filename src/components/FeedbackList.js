db5b5ec69c4a54479954cf1b5eac33729c7d6cf9
Rob Merrill <robgmerrill@gmail.com>
2023-03-01 14:53:43 -0800

    add framer animation to adding and removing feedback items

--- src/components/FeedbackList.js
+++ src/components/FeedbackList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import FeedbackItem from './FeedbackItem'
+import {motion, AnimatePresence} from 'framer-motion'
 
 function FeedbackList({feedback, handleDelete}) {
     console.log('feedback list', feedback)
@@ -7,12 +8,29 @@ function FeedbackList({feedback, handleDelete}) {
         return <p>No Feedback Yet...</p>
     }
     return (
-    <div className='feedback-list'>
-      {feedback.map((item) => (
-        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
-      ))}
-    </div>
-  )
+        <div className='feedback-list'>
+            <AnimatePresence>
+          {feedback.map((item) => (
+            <motion.div 
+                key={item.id}
+                initial={{opacity: 0}}
+                animate={{opacity: 1}}
+                exit={{opacity: 0}}
+            >
+             <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
+            </motion.div>
+          ))}
+          </AnimatePresence>
+        </div>
+      )
+
+//     return (
+//     <div className='feedback-list'>
+//       {feedback.map((item) => (
+//         <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
+//       ))}
+//     </div>
+//   )
 }
 
 export default FeedbackList
