1a7dcc32aab389a07042f6abecf085783233e618
Rob Merrill <robgmerrill@gmail.com>
2023-03-01 13:48:40 -0800

    Add form validation for length

--- src/components/FeedbackForm.js
+++ src/components/FeedbackForm.js
@@ -6,10 +6,24 @@ import { useState } from 'react';
 function FeedbackForm() {
 
     const [text, setText] = useState('')
+    const [btnDisabled, setBtnDisabled] = useState(true)
+    const [message, setMessage] = useState('')
 
     function handleTextChange(e) {
         console.log(e)
         setText(e)
+    
+        if (text === '') {
+            setBtnDisabled(true);
+            setMessage(null)
+        } else if (text !== '' & text.trim().length <= 10) {
+            setMessage('Please enter at least ten characters')
+            setBtnDisabled(false)
+        } else {
+            setMessage(null)
+            setBtnDisabled(false)
+        }
+
     }
 
     console.log(text)
@@ -21,8 +35,9 @@ function FeedbackForm() {
             {/* @todo -rating select component */}
             <div className='input-group'>
                 <input type='text' placeholder='Write a review' onChange={(e) => handleTextChange(e.target.value)} value={text} />
-                <Button type='submit' version='primary' >Send</Button>
+                <Button type='submit' version='primary' isDisabled={btnDisabled} >Send</Button>
             </div>
+            {message && <div className='message'>{message}</div>}
         </form>
     </Card>
   )
