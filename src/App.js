60e2c0a4983815566c86ea6cfd776e506a6ea22a
Rob Merrill <robgmerrill@gmail.com>
2023-03-02 08:27:10 -0800

    Fix deployment bugs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList';
-import { useState } from 'react';
-import FeedbackData from './data/FeedbackData';
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm';
 // import {v4 as uuidv4} from 'uuid'
